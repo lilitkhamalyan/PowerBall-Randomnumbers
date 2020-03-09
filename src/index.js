b0d4e4ea6c1e83c9842f409c58e6e70612bca86a
Lilit Khamalyan <lilitkhamalyan@gmail.com>
2020-03-09 12:12:01 -0400

    Created circles. We can see numbers in each circle.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import'./index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Circle extends React.Component {
+  render() {
+    return(
+      <button className="dot">
+        {this.props.value}
+    </button>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class CircleRow extends React.Component {
+  renderCircle(i) {
+    return <Circle value={i} />;
+  }
+
+  render() {
+    const status = 'Random numbers';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="circle-row">
+          {this.renderCircle(0)}
+          {this.renderCircle(1)}
+          {this.renderCircle(2)}
+          {this.renderCircle(3)}
+          {this.renderCircle(4)}
+          {this.renderCircle(5)}
+      </div>
+    </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <CircleRow />,
+  document.getElementById('root')
+);
